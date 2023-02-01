20fe07c5bea822619b5706253671e0051fcd9dde
Ulanbekkn <121215663+Ulanbekkn@users.noreply.github.com>
2023-02-01 23:45:44 +0600

    added openai

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,32 +1,36 @@
 from config import bot, ADMINS
 from aiogram import types, Dispatcher
 from random import choice
+from test import get_message
+
 
 async def echo(message: types.Message):
-    bad_words = ["дурак", "fuck", "еблан", "эшек"]
-    username = f"@{message.from_user.username}" \
-        if message.from_user.username is not None else message.from_user.first_name
-    for word in bad_words:
-        if word in message.text.lower().replace(" ", ""):
-            if message.from_user.id not in ADMINS:
-                await message.answer(f"Не матерись {username}")
-                await bot.send_message(message.from_user.id, f"Сам {message.text}\n"
-                                                             f"Не пиши такое в группу!")
-            else:
-                await message.answer("Вам можно, мой хозяин")
+    message.answer(get_message(message))
+    # bad_words = ["дурак", "fuck", "еблан", "эшек"]
+    # username = f"@{message.from_user.username}" \
+    #     if message.from_user.username is not None else message.from_user.first_name
+    # for word in bad_words:
+    #     if word in message.text.lower().replace(" ", ""):
+    #         if message.from_user.id not in ADMINS:
+    #             await message.answer(f"Не матерись {username}")
+    #             await bot.send_message(message.from_user.id, f"Сам {message.text}\n"
+    #                                                          f"Не пиши такое в группу!")
+    #         else:
+    #             await message.answer("Вам можно, мой хозяин")
+    #
+    # # if message.text.startswith("."):
+    # #     await bot.pin_chat_message(message.chat.id, message.message_id)
+    #
+    # if message.text.lower() == "dice":
+    #     await bot.send_dice(message.chat.id, emoji="⚽️")
+    #
+    # if message.text.lower().startswith("game"):
+    #     if message.from_user.id in ADMINS:
+    #         animation = choice(["⚽️", "🏀", "🎯", "🎳", "🎰", "🎲"])
+    #         await bot.send_dice(message.chat.id, emoji=animation)
+    #     else:
+    #         await message.answer(f"У вас нет прав администатора!")
 
-    # if message.text.startswith("."):
-    #     await bot.pin_chat_message(message.chat.id, message.message_id)
-
-    if message.text.lower() == "dice":
-        await bot.send_dice(message.chat.id, emoji="⚽️")
-
-    if message.text.lower().startswith("game"):
-        if message.from_user.id in ADMINS:
-            animation = choice(["⚽️", "🏀", "🎯", "🎳", "🎰", "🎲"])
-            await bot.send_dice(message.chat.id, emoji=animation)
-        else:
-            await message.answer(f"У вас нет прав администатора!")
 
 def register_handlers_extra(dp: Dispatcher):
     dp.register_message_handler(echo)
---
+++ test.py
@@ -0,0 +1,16 @@
+import openai
+from decouple import config
+
+openai.api_key = config("OPENAI_API_KEY")
+
+
+def get_message(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+    )
